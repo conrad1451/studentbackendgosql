48dbe35c8c646cd61d8e9eeb6a2e9d2c952bee01
Conrad Hansen-Quartey <Chansenq@gmail.com>
2025-08-24 07:32:16 -0400

    chore: spacing

--- main.go
+++ main.go
@@ -266,4 +266,3 @@ func deleteStudent(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]string{"message": "Student deleted successfully"})
 }
-	
